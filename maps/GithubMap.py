a498f972e50f2512b7b6113c7aecc7ac72975408
dade <dade@tutanota.com>
2016-10-12 23:59:06 -0700

    GithubMap has to exist so that we can communicate with github apis

--- maps/GithubMap.py
+++ maps/GithubMap.py
@@ -1,1 +1,17 @@
 # Github api calls for common tasks
+class GithubMap:
+	
+	def __init__(self):
+		return
+
+	def getUser(self, user):
+		# API call to fetch user
+		return 0
+
+	def getOrganization(self, org):
+		# API call to fetch organization
+		return 0
+
+	def getRepository(self, repo, sinceCommit=None):
+		# API call to fetch repository
+		return 0
