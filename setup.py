ebacf96d7755380462d4d2f7ddb5dc97c758eee5
dade <dade@tutanota.com>
2016-10-25 19:10:26 -0700

    Enabled dependency auto-installation.

--- setup.py
+++ setup.py
@@ -8,4 +8,34 @@
 
 # Set persistent information in config
 # including default api, crate type, database credentials if applicable, lists of api keys, update interval
+import pip
 
+class Setup:
+
+	def __init__(self):
+		# Do all the setup on setup instantiation
+		self.setupDependencies()
+		self.setupConfig()
+		self.setupCrate("MySQL")
+		return
+
+	def setupCrate(self, crateType):
+		# initialize the crate matching crateType
+		return
+	
+	def setupConfig(self):
+		# Ask the user for details about how they are going to run the tool
+		# Save this to ~/.gitloot
+		return
+
+	def setupDependencies(self):
+		# pip install the requirements.txt file
+		pip.main(['install', '-r', 'requirements.txt'])
+		return
+
+def main():
+	# setup.py can run standalone or run automatically at first run.
+	setup = Setup()
+
+if __name__ == '__main__':
+	main()
