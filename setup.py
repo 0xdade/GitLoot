2636f1247f5de47e9d4a07a224b49689b9c61d85
dade <dade@tutanota.com>
2016-10-25 19:37:09 -0700

    Enabling us to actually create a config file so you can get past setup

--- setup.py
+++ setup.py
@@ -9,6 +9,8 @@
 # Set persistent information in config
 # including default api, crate type, database credentials if applicable, lists of api keys, update interval
 
+
+
 class Setup:
 
 	def __init__(self):
@@ -21,10 +23,28 @@ class Setup:
 	def setupCrate(self, crateType):
 		# initialize the crate matching crateType
 		return
-	
+
 	def setupConfig(self):
 		# Ask the user for details about how they are going to run the tool
 		# Save this to ~/.gitloot
+		print "What type of API is this? (github)"
+		api_type = raw_input("API Type: ")
+		print "Please let us know the root of the API that we'll be running against."
+		api_root = raw_input("API Root: ")
+		if api_root[-1] != "/":
+			api_root = api_root + "/"
+		print "Please enter API tokens as a comma separated list"
+		api_tokens = raw_input("API Token(s): ")
+		api_tokens = api_tokens.strip(" ")
+		print "What user agent would you like to use?"
+		user_agent = raw_input("User-Agent: ")
+		import os
+		configFile = os.path.expanduser('~') + "/.gitloot"
+
+		if api_type == "github":
+			confHeader = "[GithubMap]\n"
+		with open(configFile, 'w+') as conf:
+			conf.write(confHeader + "api_root=\"" + api_root + "\"\napi_tokens=\"" + api_tokens + "\"\nuser-agent=\"" + user_agent + "\"\n")
 		return
 
 	def setupDependencies(self):
